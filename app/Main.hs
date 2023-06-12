56492521711f3d1befe97d85a905894792d58e5d
Joshua Hayes <joshjohayes@gmail.com>
2023-06-12 14:08:13 +0100

    pass stage 2

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,17 @@
 module Main where
 
+import Data.Char
 import System.Environment
 import System.Exit
 
 matchPattern :: String -> String -> Bool
 matchPattern pattern input = do
-  if length pattern == 1
-    then head pattern `elem` input
-    else error $ "Unhandled pattern: " ++ pattern
+  if pattern == "\\d"
+    then any isDigit input
+    else
+      if length pattern == 1
+      then head pattern `elem` input
+      else error $ "Unhandled pattern: " ++ pattern
 
 main :: IO ()
 main = do
@@ -23,6 +27,7 @@ main = do
     then do
       putStrLn "Expected first argument to be '-E'"
       exitFailure
-    else do if matchPattern pattern input_line
-              then exitSuccess
-              else exitFailure
+    else
+      if matchPattern pattern input_line
+      then exitSuccess
+      else exitFailure
